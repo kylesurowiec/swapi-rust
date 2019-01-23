9f0114f6738502b7d4923ee10eb61cf83fb4240f
Kyle Surowiec <kylesurowiec@me.com>
2019-01-22 20:44:15 -0600

    Verbiage update (#19)
    * changing around verbiage for response types | moving default struct creation inside library functions for simplicity
    
    * finished verbiage update and fixed issue #10

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 extern crate swapi;
 
 fn main() {
-    let mut planet_resp: swapi::types::Species = Default::default();
-    swapi::query_species("6", &mut planet_resp);
-    println!("{:#?}", planet_resp);
+    swapi::query_species("2");
 }
